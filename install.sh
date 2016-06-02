564402a0256953b0825b57028ddaa05a3c63e3b7
EmilioFigueras <figmare@hotmail.com>
2016-06-02 16:15:14 +0200

    Actualización install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 echo "*** Instalación de el proyecto web IW2016-UJ ***"
-
 echo "Obteniendo proyecto del repositorio git"
-
 git clone https://github.com/IngenieriaWebUCA/iw2016-UJ.git
-cd iw2016-uj/
-
+cd iw2016-UJ/
 echo "Creación de la base de datos."
-
 read -p "Nombre de usuario con permisos de administrador de la base de datos:" usuario
 read -s -p "Contraseña del usuario:" pass
-
-mysql -u $usuario -p $pass < iw2016UJ.sql
-
-echo "Aplicación desplegada. Dirijase a http://localhost:8080/UJ_2016"
-
+mysql -u $usuario -p $pass < Database/iw2016UJ.sql
+echo "Aplicación desplegada. Dirijase a http://localhost:8080/iw2016-UJ/UJ_2016"
 echo "Desplegando aplicación..."
-mvn tomcat7:run
+cd UJ_2016/
+sudo mvn tomcat7:run
