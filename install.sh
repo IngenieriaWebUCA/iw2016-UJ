a793beb3d8723068b3adf2f81defb7ffa717de71
root <root@emilio.debian>
2016-06-02 18:15:02 +0200

    /n y otras actualizaciones del install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-echo "*** Instalación de el proyecto web IW2016-UJ ***"
-echo "Obteniendo proyecto del repositorio git"
-git clone https://github.com/IngenieriaWebUCA/iw2016-UJ.git
-cd iw2016-UJ/
 echo "Creación de la base de datos."
 read -p "Nombre de usuario BD:" usuario
 mysql -u $usuario -p < Database/iw2016UJ.sql
