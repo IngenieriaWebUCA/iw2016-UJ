ab5627da0aae89ec4e0e14a14033c2c2422017ba
IvanSalguero <ivan.salgueroastete@alum.uca.es>
2016-06-02 17:41:39 +0200

    Actualización install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,8 @@ echo "Obteniendo proyecto del repositorio git"
 git clone https://github.com/IngenieriaWebUCA/iw2016-UJ.git
 cd iw2016-UJ/
 echo "Creación de la base de datos."
-read -p "Nombre de usuario con permisos de administrador de la base de datos:" usuario
-read -s -p "Contraseña del usuario:" pass
-mysql -u $usuario -p $pass < Database/iw2016UJ.sql
+read -p "Nombre de usuario BD:" usuario
+mysql -u $usuario -p < Database/iw2016UJ.sql
 echo "Aplicación desplegada. Dirijase a http://localhost:8080/iw2016-UJ/UJ_2016"
 echo "Desplegando aplicación..."
 cd UJ_2016/
