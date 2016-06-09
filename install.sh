633613ba1cd749dde4963906bb7f476a203848cf
root <root@emilio.debian>
2016-06-09 16:46:43 +0200

    Script y acceso a BD

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Creación de la base de datos."
-read -p "Nombre de usuario BD:" usuario
+read -p "Nombre de usuario ya existene en la BD con permisos de administración (root):" usuario
 mysql -u $usuario -p < Database/iw2016UJ.sql
 echo "Aplicación desplegada. Dirijase a http://localhost:8080/iw2016-UJ/UJ_2016"
 echo "Desplegando aplicación..."
